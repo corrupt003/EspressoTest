c64030bf699fcc99d642c92a73d80ae2af602ce9
corrupt003 <corrupt003@gmail.com>
2019-07-23 22:06:50 +0800

    Init Android project.

---
+++ app/src/androidTest/java/android/com/espressotest/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package android.com.espressotest
+
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = androidx.test.core.app.ApplicationProvider.getApplicationContext<Context>()
+        assertEquals("android.com.espressotest", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/android/com/espressotest/OnViewActivity.kt
@@ -0,0 +1,12 @@
+package android.com.espressotest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OnViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
