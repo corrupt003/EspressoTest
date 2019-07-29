876d7706a2f9c908a32eaa597f3220264d28fc33
yuan_chen <corrupt003@gmail.com>
2019-07-29 19:02:21 +0800

    Add two activities.
    1. Two Activity can share data using Intent.

---
+++ app/src/main/java/com/android/espressotest/OnViewActivity1.kt
@@ -0,0 +1,86 @@
+package com.android.espressotest
+
+import android.com.espressotest.R
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class OnViewActivity1 : AppCompatActivity() {
+
+    companion object {
+        // Class name for Log tag
+        private val LOG_TAG = OnViewActivity1::class.java.simpleName
+        // Unique tag required for the intent extra
+        const val EXTRA_MESSAGE = "com.android.espressotest.extra.MESSAGE"
+        // Unique tag for the intent reply
+        private const val TEXT_REQUEST = 1
+    }
+
+    // EditText view for the message
+    private var mMessageEditText: EditText? = null
+    // TextView for the reply header
+    private var mReplyHeadTextView: TextView? = null
+    // TextView for the reply body
+    private var mReplyTextView: TextView? = null
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onview1)
+
+        // Initialize all the view variables.
+        mMessageEditText = findViewById(R.id.editText_main)
+        mReplyHeadTextView = findViewById(R.id.text_header_reply)
+        mReplyTextView = findViewById(R.id.text_message_reply)
+    }
+
+    /**
+     * Handles the onClick for the "Send" button. Gets the value of the main EditText,
+     * creates an intent, and launches the second activity with that intent.
+     *
+     * The return intent from the second activity is onActivityResult().
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    fun launchSecondActivity(view: View) {
+        Log.d(LOG_TAG, "Button clicked!")
+        val intent = Intent(this, OnViewActivity2::class.java)
+        val message = mMessageEditText!!.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
+        startActivityForResult(intent, TEXT_REQUEST)
+    }
+
+    /**
+     * Handles the data in the return intent from SecondActivity.
+     *
+     * @param requestCode Code for the SecondActivity request.
+     * @param resultCode Code that comes back from SecondActivity.
+     * @param data Intent data sent back from SecondActivity.
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Test for the right intent reply.
+        if (requestCode == TEXT_REQUEST) {
+            // Test to make sure the intent reply result was good.
+            if (resultCode == RESULT_OK) {
+                val reply = data?.getStringExtra(OnViewActivity2.EXTRA_REPLY)
+
+                // Make the reply head visible.
+                mReplyHeadTextView!!.visibility = View.VISIBLE
+
+                // Set the reply and make it visible.
+                mReplyTextView!!.text = reply
+                mReplyTextView!!.visibility = View.VISIBLE
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/android/espressotest/OnViewActivity2.kt
@@ -0,0 +1,60 @@
+package com.android.espressotest
+
+import android.com.espressotest.R
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class OnViewActivity2 : AppCompatActivity() {
+    companion object {
+        // Unique tag for the intent reply.
+        const val EXTRA_REPLY = "com.android.espressotest.extra.REPLY"
+    }
+
+    // EditText for the reply.
+    private var mReply: EditText? = null
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onview2)
+
+        // Initialize view variables.
+        mReply = findViewById(R.id.editText_second)
+
+        // Get the intent that launched this activity, and the message in
+        // the intent extra.
+        val intent = intent
+        val message = intent.getStringExtra(OnViewActivity1.EXTRA_MESSAGE)
+
+        // Put that message into the text_message TextView
+        val textView: TextView = findViewById(R.id.text_message)
+        textView.setText(message)
+    }
+
+    /**
+     * Handles the onClick for the "Reply" button. Gets the message from the
+     * second EditText, creates an intent, and returns that message back to
+     * the main activity.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    fun returnReply(view: View) {
+        // Get the reply message from the edit text.
+        val reply = mReply!!.text.toString()
+
+        // Create a new intent for the reply, add the reply message to it
+        // as an extra, set the intent result, and close the activity.
+        val replyIntent = Intent()
+        replyIntent.putExtra(EXTRA_REPLY, reply)
+        setResult(RESULT_OK, replyIntent)
+        finish()
+    }
+}
